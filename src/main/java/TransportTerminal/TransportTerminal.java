fac1415d3210622bc532dd753f4ecc67e020b7d4
ganymedes01 <foka_12@hotmail.com>
2014-09-01 23:41:01 +0100

    Textures are named wrong

--- src/main/java/TransportTerminal/TransportTerminal.java
+++ src/main/java/TransportTerminal/TransportTerminal.java
@@ -16,30 +16,31 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 @Mod(modid = "transportterminal", name = "Transport Terminal", version = "0.99a")
 public class TransportTerminal {
-	
+
 	@Instance("transportterminal")
 	public static TransportTerminal instance;
-	
+
 	@SidedProxy(clientSide = "TransportTerminal.ClientProxyTransportTerminal", serverSide = "TransportTerminal.CommonProxyTransportTerminal")
 	public static CommonProxyTransportTerminal proxy;
+
 	public static Item transportTerminalRemote;
 	public static Item transportTerminalChip;
 	public static Block transportTerminal;
 	public static SimpleNetworkWrapper networkWrapper;
-	
+
 	@EventHandler
 	public void Init(FMLInitializationEvent event) {
 		proxy.registerRenderInformation();
 		proxy.registerTileEntities();
-		
-		transportTerminalRemote = new ItemTransportTerminalRemote().setUnlocalizedName("transportTerminalRemote").setTextureName("transportTerminal:transportTerminalRemote");
-		transportTerminalChip = new ItemTransportTerminalChip().setUnlocalizedName("transportTerminalChip").setTextureName("transportTerminal:transportTerminalChipBlank");
-		transportTerminal = new BlockTransportTerminal().setHardness(3.0F).setBlockName("transportTerminal").setBlockTextureName("transportTerminal:transportTerminal");
+
+		transportTerminalRemote = new ItemTransportTerminalRemote().setUnlocalizedName("transportTerminalRemote").setTextureName("transportterminal:transportTerminalRemote");
+		transportTerminalChip = new ItemTransportTerminalChip().setUnlocalizedName("transportTerminalChip").setTextureName("transportterminal:transportTerminalChipBlank");
+		transportTerminal = new BlockTransportTerminal().setHardness(3.0F).setBlockName("transportTerminal").setBlockTextureName("transportterminal:transportTerminal");
 
 		GameRegistry.registerItem(transportTerminalRemote, "Transport Terminal Remote");
 		GameRegistry.registerItem(transportTerminalChip, "Transport Terminal Chip");
 		GameRegistry.registerBlock(transportTerminal, "Transport Terminal");
-		
+
 		TransportTerminalCrafting.addRecipes();
 	}
 
@@ -48,7 +49,7 @@ public class TransportTerminal {
 	public void preInit(FMLPreInitializationEvent event) {
 		NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
 		networkWrapper = NetworkRegistry.INSTANCE.newSimpleChannel("transportterminal");
-		networkWrapper.registerMessage(TeleportPacketHandler.class, TeleportMessage.class, 0, Side.SERVER); 
+		networkWrapper.registerMessage(TeleportPacketHandler.class, TeleportMessage.class, 0, Side.SERVER);
 		networkWrapper.registerMessage(NamingPacketHandler.class, NamingMessage.class, 1, Side.SERVER);
 	}
 }
