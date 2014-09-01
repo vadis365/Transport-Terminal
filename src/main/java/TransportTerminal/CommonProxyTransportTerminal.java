754acacadc11c60fcc3e52e5cb0ac4cead3563a3
vadis365 <curve_linear@hotmail.com>
2014-09-01 22:13:12 +0100

    removed some stuff

--- src/main/java/TransportTerminal/CommonProxyTransportTerminal.java
+++ src/main/java/TransportTerminal/CommonProxyTransportTerminal.java
@@ -3,7 +3,6 @@ package TransportTerminal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import net.minecraftforge.common.DimensionManager;
 import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -32,7 +31,6 @@ public class CommonProxyTransportTerminal implements IGuiHandler{
 		}
 		
 		if (ID == GUI_ID_REMOTE) {
-			world = DimensionManager.getWorld(player.getCurrentEquippedItem().getTagCompound().getInteger("dim"));
 			TileEntity tileentity = world.getTileEntity(x, y, z);
 			if (tileentity instanceof TileEntityTransportTerminal) {
 				return new ContainerTerminal(player.inventory, (TileEntityTransportTerminal) tileentity, 1);
@@ -51,7 +49,6 @@ public class CommonProxyTransportTerminal implements IGuiHandler{
 		}
 	
 		if (ID == GUI_ID_REMOTE) {
-			world = DimensionManager.getWorld(player.getCurrentEquippedItem().getTagCompound().getInteger("dim"));
 			TileEntity tileentity = world.getTileEntity(x, y, z);
 			if (tileentity instanceof TileEntityTransportTerminal) {
 				return new GuiNaming(player.inventory, (TileEntityTransportTerminal) tileentity);
