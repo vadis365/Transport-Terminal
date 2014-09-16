46861ea19bf7a57ab29f063fb2501451d89d2bff
vadis365 <curve_linear@hotmail.com>
2014-09-16 01:12:08 +0100

    Item sender not done.....
    Dimension travel borked

--- src/main/java/TransportTerminal/network/TeleportPacketHandlerItems.java
+++ src/main/java/TransportTerminal/network/TeleportPacketHandlerItems.java
@@ -19,7 +19,7 @@ public class TeleportPacketHandlerItems implements IMessageHandler<TeleportMessa
 	@Override
 	public IMessage onMessage(TeleportMessageItems message, MessageContext ctx) {
 		WorldServer world = DimensionManager.getWorld(message.dimension);
-		
+
 		if (world == null)
 			return null;
 		
@@ -38,9 +38,12 @@ public class TeleportPacketHandlerItems implements IMessageHandler<TeleportMessa
 					MinecraftServer mcServer = MinecraftServer.getServer();
 					EntityItem entityitem = new EntityItem(world, message.tileX + 0.5D, message.tileY + 1.5D, message.tileZ + 0.5D, is);
 					entityitem.delayBeforeCanPickup = 20;
-					entityitem.travelToDimension(message.chipDim);
-					entityitem.setLocationAndAngles(message.chipX + 0.5D, message.chipY + 1D, message.chipZ + 0.5D, entityitem.rotationYaw, entityitem.rotationPitch);
+					entityitem.worldObj.removeEntity(entityitem);
+					mcServer.getConfigurationManager().transferEntityToWorld(entityitem, message.chipDim, world, world2, new TransportTerminalTeleporter(world2));
 					entityitem.isDead = false;
+					entityitem.setLocationAndAngles(message.chipX + 0.5D, message.chipY + 1D, message.chipZ + 0.5D, entityitem.rotationYaw, entityitem.rotationPitch);
+					world2.spawnEntityInWorld(entityitem);
+					entityitem.worldObj.updateEntity(entityitem);
 					world.playSoundEffect(message.tileX, message.tileY, message.tileZ, "transportterminal:teleportsound", 1.0F, 1.0F);
 					world2.playSoundEffect(message.chipX, message.chipY, message.chipZ, "transportterminal:teleportsound", 1.0F, 1.0F);
 					postBox.setInventorySlotContents(1, null);
