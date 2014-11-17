46c49c07570b98a40990dd2447bd67de5cd0db75
ganymedes01 <foka_12@hotmail.com>
2014-11-17 21:37:07 -0200

    Allow player to open terminal's gui holding whatever item he wants
    Why did we block the player form opening the gui if he had a remote or
    was holding another terminal?

--- src/main/java/transportterminal/blocks/BlockTransportTerminal.java
+++ src/main/java/transportterminal/blocks/BlockTransportTerminal.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MathHelper;
@@ -53,13 +52,8 @@ public class BlockTransportTerminal extends BlockContainer {
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
 		if (world.isRemote)
 			return true;
-		TileEntity tile = world.getTileEntity(x, y, z);
-		if (tile != null && tile instanceof TileEntityTransportTerminal) {
-			ItemStack current = player.inventory.getCurrentItem();
-			if (current != null && current.getItem() == Item.getItemFromBlock(this) || current != null && current.getItem() == TransportTerminal.remote)
-				return false;
+		if (world.getTileEntity(x, y, z) != null)
 			player.openGui(TransportTerminal.instance, TransportTerminal.proxy.GUI_ID_TERMINAL, world, x, y, z);
-		}
 		return true;
 	}
 
