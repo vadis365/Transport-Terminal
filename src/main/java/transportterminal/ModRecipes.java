914698fa749e9636558cbb628aa3f9761a353ea8
vadis365 <curve_linear@hotmail.com>
2014-11-22 21:19:54 +0000

    recipe

--- src/main/java/transportterminal/ModRecipes.java
+++ src/main/java/transportterminal/ModRecipes.java
@@ -21,6 +21,7 @@ public class ModRecipes {
 		addShapelessRecipe(nbtPlayerChip, Items.name_tag, "gemDiamond", TransportTerminal.chip);
 
 		addShapedRecipe(new ItemStack(TransportTerminal.remote), "RER", " C ", "IBI", 'R', "dustRedstone", 'E', Items.ender_pearl, 'C', TransportTerminal.chip, 'I', "ingotIron", 'B', Blocks.stone_button);
+		addShapedRecipe(new ItemStack(TransportTerminal.remoteTerminal), "IGI", "RXR", "ICI", 'R', "dustRedstone", 'G', "blockGlass", 'C', TransportTerminal.chip, 'I', "ingotIron", 'X', TransportTerminal.remote);
 		addShapedRecipe(new ItemStack(TransportTerminal.terminal), "EGE", "IRI", "SSS", 'E', Items.ender_eye, 'G', "blockGlass", 'I', "blockIron", 'R', "blockRedstone", 'S', Blocks.stone_slab);
 		addShapedRecipe(new ItemStack(TransportTerminal.utils), "CTC", "IRI", "xxx", 'C', TransportTerminal.chip, 'T', Items.name_tag, 'I', "blockIron", 'R', "blockRedstone", 'x', "ingotIron");
 		addShapedRecipe(new ItemStack(TransportTerminal.charger), "CRC", "GQG", "CLC", 'C', TransportTerminal.chip, 'G', "blockGold", 'Q', "blockQuartz", 'R', "blockRedstone", 'L', "blockLapis");
