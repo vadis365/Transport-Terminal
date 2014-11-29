eaf05d638508699fc0d8222fde4409c20eff2fb5
vadis365 <curve_linear@hotmail.com>
2014-11-29 22:02:43 +0000

    stuff

---
+++ src/main/java/transportterminal/blockutils/BlockPosUtils.java
@@ -0,0 +1,17 @@
+package transportterminal.blockutils;
+
+import net.minecraft.block.Block;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockPosUtils {
+	
+	public static Block getBlock(World world, int x, int y, int z, BlockPos pos) {
+		return world.getBlockState(pos.add(x - pos.getX(), y - pos.getY(), z - pos.getZ())).getBlock();
+	}
+
+	public static void setBlock(World world, int x, int y, int z, BlockPos pos, Block block) {
+		world.setBlockState(pos.add(x - pos.getX(), y - pos.getY(), z - pos.getZ()), block.getDefaultState());
+	}
+
+}
