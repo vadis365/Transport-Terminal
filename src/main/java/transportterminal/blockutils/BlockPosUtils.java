144785e9693010f8fde866463784d603b952450d
ganymedes01 <foka_12@hotmail.com>
2014-11-29 20:26:57 -0200

    Clean up

--- src/main/java/transportterminal/blockutils/BlockPosUtils.java
+++ src/main/java/transportterminal/blockutils/BlockPosUtils.java
@@ -5,7 +5,7 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 public class BlockPosUtils {
-	
+
 	public static Block getBlock(World world, int x, int y, int z, BlockPos pos) {
 		return world.getBlockState(pos.add(x - pos.getX(), y - pos.getY(), z - pos.getZ())).getBlock();
 	}
@@ -13,5 +13,4 @@ public class BlockPosUtils {
 	public static void setBlock(World world, int x, int y, int z, BlockPos pos, Block block) {
 		world.setBlockState(pos.add(x - pos.getX(), y - pos.getY(), z - pos.getZ()), block.getDefaultState());
 	}
-
 }
