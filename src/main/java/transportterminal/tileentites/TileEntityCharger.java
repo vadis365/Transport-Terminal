77cb649621c73225797d5827220ed7f16c3aaa96
vadis365 <curve_linear@hotmail.com>
2016-07-12 23:44:30 +0100

    Battery Test
    Allows Item Charger to output energy from all sides and power machines -
    a test for possible future features

--- src/main/java/transportterminal/tileentites/TileEntityCharger.java
+++ src/main/java/transportterminal/tileentites/TileEntityCharger.java
@@ -4,12 +4,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import transportterminal.core.confighandler.ConfigHandler;
 import transportterminal.utils.TeleportUtils;
 import cofh.api.energy.IEnergyContainerItem;
+import cofh.api.energy.IEnergyHandler;
+import cofh.api.energy.IEnergyReceiver;
 
 public class TileEntityCharger extends TileEntityInventoryEnergy implements ITickable {
 
@@ -42,6 +44,17 @@ public class TileEntityCharger extends TileEntityInventoryEnergy implements ITic
 				extractEnergy(null, received, false);
 				TeleportUtils.consumeChargerEnergy(this, received);
 				stored = getEnergyStored(null);
+			}
+		}
+		
+		if ((stored > 0)) {
+			for (EnumFacing facing : EnumFacing.VALUES) {
+				TileEntity tile = worldObj.getTileEntity(pos.offset(facing));
+				if (tile != null && tile instanceof IEnergyHandler) {
+					int received = ((IEnergyReceiver)tile).receiveEnergy(facing.getOpposite(), stored, false);
+					extractEnergy(facing, received, false);
+					TeleportUtils.consumeChargerEnergy(this, received);
+				}
 			}
 		}
 	}
